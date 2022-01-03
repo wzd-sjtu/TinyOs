2b434a806e79a62b63cd6a1159cd95b4d280e151
wzd <wzdwxjwxw@sjtu.edu.cn>
2022-01-03 10:38:45 +0800

    formaly complete my print function

---
+++ 6.MyPrintFunction/kernal/main.c
@@ -0,0 +1,18 @@
+#include "print.h"
+int main(void) {
+    
+    put_char('w');
+    put_char('z');
+    put_char('d');
+    put_char(' ');
+    put_char('h');
+    put_char('a');
+    put_char('p');
+    put_char('p');
+    put_char('y');
+    put_char('!');
+    
+
+    while(1);
+    return 0;
+}
