1505add13303b5c7fb203a05db772e8363180eb5
wzd <wzdwxjwxw@sjtu.edu.cn>
2022-01-11 21:55:38 +0800

    no use document

---
+++ Deleted_document/6.MyPrintFunction/kernal/main.c
@@ -0,0 +1,22 @@
+#include "print.h"
+int main(void) {
+    
+    /*
+    put_char('w');
+    put_char('z');
+    put_char('d');
+    put_char(' ');
+    put_char('h');
+    put_char('a');
+    put_char('p');
+    put_char('p');
+    put_char('y');
+    put_char('!');
+    put_char('\n');
+    put_char('w');
+    */
+
+    put_str("wzd is handsome!\nwzd is also beautiful!\n");
+    while(1);
+    return 0;
+}
